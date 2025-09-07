e62831071c03fdd5566d61924c7acdd8c9e3feec
Jyoti Prakash Panda <96424607+jyotiprakashh@users.noreply.github.com>
2025-09-08 02:24:15 +0530

    fix: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,22 @@ import Navbar from "../components/fixed-navbar";
 export const metadata: Metadata = {
   title: "Jyoti Prakash Panda",
   description: "Portfolio of Jyoti Prakash Panda",
+  keywords: ["Jyoti Prakash Panda", "Portfolio", "Jyoti Prakash Panda Portfolio"],
+  openGraph: {
+    title: "Jyoti Prakash Panda",
+    description: "Portfolio of Jyoti Prakash Panda",
+    type: "website",
+    url: "https://jyotiprakashpanda.vercel.app",
+    siteName: "Jyoti Prakash Panda",
+    images: [
+      {
+        url: "https://jyotiprakashpanda.vercel.app/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Jyoti Prakash Panda",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
